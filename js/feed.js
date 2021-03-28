410ee3e632863eecc4d2c923b1fc0d60d3e5fdfa
Anna Humeniuk <aznosenko@gmail.com>
2021-03-28 19:44:59 +0300

    8.1

--- js/feed.js
+++ js/feed.js
@@ -7,55 +7,49 @@ const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 const filterBlock = document.querySelector('.img-filters');
-const filterDefaultButton = document.querySelector('#filter-default');
-const filterRandomButton = document.querySelector('#filter-random');
-const filterDiscussedButton = document.querySelector('#filter-discussed');
 
 //должна брать данные из параметров
 const RENDER_DELAY = 500;
 
-const makePic = (photos) => {
-  const renderPhotos = (photos) => {
-    photos.forEach((photo) => {
-      const pictureElement = pictureTemplate.cloneNode(true);
-      pictureElement.querySelector('.picture__img')
-        .src = photo.url;
-      pictureElement.querySelector('.picture__info')
-        .querySelector('.picture__likes')
-        .textContent = photo.likes;
-      pictureElement.querySelector('.picture__info')
-        .querySelector('.picture__comments')
-        .textContent = photo.comments.length;
-      pictureList.appendChild(pictureElement);
-      showBigPicture(pictureElement, photo);
-    })
-  };
+const renderPhotos = (photos) => {
+  photos.forEach((photo) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img')
+      .src = photo.url;
+    pictureElement.querySelector('.picture__info')
+      .querySelector('.picture__likes')
+      .textContent = photo.likes;
+    pictureElement.querySelector('.picture__info')
+      .querySelector('.picture__comments')
+      .textContent = photo.comments.length;
+    pictureList.appendChild(pictureElement);
+    showBigPicture(pictureElement, photo);
+  })
+};
 
+const makePic = (photos) => {
   renderPhotos(photos);
   filterBlock.classList.remove('img-filters--inactive');
+  filterBlock.addEventListener('click',
+    _.debounce((evt) => clickFilter(evt, photos), RENDER_DELAY))
+};
 
-  const clearPhotoList = () => {
-    document.querySelectorAll('.picture')
-      .forEach(el => el.remove());
-  };
+const clearPhotoList = () => {
+  document.querySelectorAll('.picture')
+    .forEach(el => el.remove());
+};
 
-  filterDefaultButton.addEventListener('click', _.debounce(() => {
+const clickFilter = (evt, photos) => {
+  const filter = evt.target;
+  if (filter.matches('.img-filters__button')) {
+    if (filter.matches('#filter-random')) {
+      photos = _.sampleSize(photos, 10);
+    } else if (filter.matches('#filter-discussed')) {
+      photos = _.sortBy(photos, 'comments.length').reverse();
+    }
     clearPhotoList();
     renderPhotos(photos);
-  }, RENDER_DELAY));
-
-  filterRandomButton.addEventListener('click', _.debounce(() => {
-    const randomPhotos = _.sampleSize(photos, 10);
-    clearPhotoList();
-    renderPhotos(randomPhotos);
-  }, RENDER_DELAY));
-
-  filterDiscussedButton.addEventListener('click', _.debounce(() => {
-    const discussedPhotos = _.sortBy(photos, 'comments.length').reverse();
-    clearPhotoList();
-    renderPhotos(discussedPhotos);
-  }, RENDER_DELAY));
-
+  }
 };
 
 
